36b1b13f104875f2d0f0ab25a3c53e8f8073db5c
sara bhaby <sara.bhaby@gmail.com>
2023-04-21 13:04:19 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,11 +10,10 @@
 
 int main(void)
 {
-	char c;
-	char nl = '\n';
+	int n;
 
-	for (c = '0'; c <= '9'; c++)
-		putchar(c);
-	putchar(nl);
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
 	return (0);
 }
