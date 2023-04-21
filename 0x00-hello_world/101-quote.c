c99ba85e570fef864db8e0cba7adc600a6284bd0
sara bhaby <sara.bhaby@gmail.com>
2023-04-21 10:51:57 -0700

    fwrite

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Error)
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		sizeof(char), 59,  stderr);
+	return (1);
+}
