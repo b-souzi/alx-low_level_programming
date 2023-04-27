413719774c4c91be4e7e6de0c10e5c36562ba146
sara bhaby <sara.bhaby@gmail.com>
2023-04-27 02:16:53 -0700

    love

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+**/
+* _isdigit - check if numbers are 0 - 9
+* @c: caracter to be cheked.
+*
+* Return: '0' or '1'
+*/
+ 
+ int _isdigit(int c)
+{
+ 		if (c >= '0' && <= '9')
+			return (i);
+		else
+			return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul -  multiplies two integers
+ * @a: first int
+ * @b: second int
+ * Return: addition or multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
