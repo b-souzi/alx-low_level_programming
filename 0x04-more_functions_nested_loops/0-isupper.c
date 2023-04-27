4b06571da01071f1d9f2a7df45e29a4cc625f1bb
sara bhaby <sara.bhaby@gmail.com>
2023-04-27 07:57:07 -0700

    sara

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
  * _isupper - checks for uppercase character
  * @c: character to check
  *
- * return: 0 or 1
+ * Return: 0 or 1
  */
 
 int _isupper(int c)
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
-**/
+/**
 * _isdigit - check if numbers are 0 - 9
 * @c: caracter to be cheked
 *
 * Return: '0' or '1'
 */
- 
- int _isdigit(int c)
+
+int _isdigit(int c)
 {
- 		if (c >= '0' && <= '9')
-			return (i);
-		else
-			return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
 
