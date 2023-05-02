f23782305c0f2ccf2e67ca79c8637dabb7c30a16
sara bhaby <sara.bhaby@gmail.com>
2023-05-02 16:46:26 -0700

    pasco

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,24 @@
-#iclude "main.h"
+#include "main.h"
 
 /**
- * void print_rev(char *s) - print a string in reverse
- 
+ * print_rev - print a string in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int leni = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		leni++;
+		s++;
+	}
+	s++;
+	for (i = leni; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
