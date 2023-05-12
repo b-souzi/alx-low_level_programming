5e8021ff891e8b8e0958d1bec36eb16f51e8cb4b
sara bhaby <sara.bhaby@gmail.com>
2023-05-12 14:30:28 -0700

    liball

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c 0-*.c 100-*.c 1-*.c 2-*.c 3-*.c 4-*.c 5-*.c 6-*.c 9-*.c _*.c
+gcc -Wall -pedantic -Werror -Wextra -c *.c
 ar -rc liball.a *.o
 ranlib liball.a
