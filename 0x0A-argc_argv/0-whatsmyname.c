5be5b6d94ba33af996f8ee7bd8176d78c00121d3
sara bhaby <sara.bhaby@gmail.com>
2023-05-12 16:40:40 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print program name
+ * @argc: arguments count
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
