7e7fd65b45a9346dfb21294167955bae52c3bd22
sara bhaby <sara.bhaby@gmail.com>
2023-05-22 14:21:34 -0700

    souzi

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef structdog
+#define structdog
+/**
+ * struct dog - dog type
+ * @name: dog name
+ * @age: age dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
