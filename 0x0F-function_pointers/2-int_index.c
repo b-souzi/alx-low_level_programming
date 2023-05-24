683c48041928d2503e3b80f8f516dd6ad20ca287
sara bhaby <sara.bhaby@gmail.com>
2023-05-24 15:59:05 -0700

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Integer search
+ * @array: Array
+ * @size: Size
+ * @cmp: Function
+ * Return: Index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
