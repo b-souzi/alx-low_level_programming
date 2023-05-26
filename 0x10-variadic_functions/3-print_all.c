91558b04934528a637cecf4274f482c7c88ad682
sara bhaby <sara.bhaby@gmail.com>
2023-05-26 14:50:49 -0700

    all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,47 @@
 #include "variadic_functions.h"
 
 /**
+ * print_char - Prints a char
+ * @c: Char
+ */
+void print_char(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+ * print_integer - Prints an integer
+ * @i: Integer
+ */
+void print_integer(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * print_float - Prints a float
+ * @f: Float
+ */
+void print_float(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * print_string - Prints a string
+ * @s: String
+ */
+void print_string(va_list s)
+{
+	char *str = va_arg(s, char*);
+
+	if (str == NULL)
+		printf("(nil)");
+	else
+		printf("%s", str);
+}
+
+/**
  * print_all - Print anything
  * @format: Print format
  */
@@ -10,36 +51,32 @@
 void print_all(const char * const format, ...)
 {
 	int i = 0;
+	int j;
 	va_list ap;
-	char *str;
 	char *sep = "";
+	formats_t typd[] = {
+		{"c", print_char},
+		{"i", print_integer},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
 
 	va_start(ap, format);
-	while (format[i])
+	while (format && format[i])
 	{
-		switch (format[i])
+		j = 0;
+		while (typd[j].t != NULL)
 		{
-			case 'c':
-				printf("%s%c", sep, va_arg(ap, int));
+			if (*(typd[j].t) == format[i])
+			{
+				printf("%s", sep);
+				typd[j].f(ap);
+				sep = ", ";
 				break;
-			case 'i':
-				printf("%s%d", sep, va_arg(ap, int));
-				break;
-			case 'f':
-				printf("%s%f", sep, va_arg(ap, double));
-				break;
-			case 's':
-				str = va_arg(ap, char*);
-				if (str == NULL)
-					printf("%s(nil)", sep);
-				else
-					printf("%s%s", sep, str);
-				break;
-			default:
-				i++;
-				continue;
+			}
+			j++;
 		}
-		sep = ", ";
 		i++;
 	}
 	printf("\n");
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,17 @@
 #ifndef Variadic_Functions
 #define Variadic_Functions
+#include <stdarg.h>
+
+/**
+ * struct formats - Data format
+ * @t: Type
+ * @f: Function
+ */
+typedef struct formats
+{
+	char *t;
+	void (*f)(va_list);
+} formats_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
