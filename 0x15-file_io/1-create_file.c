df1331c8338d0073f5aee4463275db1b0a676fda
Sara BHABY <sara.bhaby@gmail.com>
2023-07-10 15:26:07 -0700

    read

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: A pointer to create
+ * @text_content: A pointer to a string
+ * Return: upon success, return 1, or -1
+*/
+
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t b_written = 0;
+	int fd;
+
+	if (!filname)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+
+	if (text_content)
+		b_written = write(fd, text_content, _strlen(text_content));
+	close(fd);
+
+	if (b_written < 0)
+		return (-1);
+	return (1);
+}
