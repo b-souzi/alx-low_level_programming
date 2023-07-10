de450fd955de570cbe4ab6d5be89e78b37ca99b7
Sara BHABY <sara.bhaby@gmail.com>
2023-07-10 15:35:25 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,19 +1,33 @@
 #include "main.h"
 
 /**
+ * _strlen - lenght of string
+ * @str: string
+ * Return: length
+ */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
+
+	if (!str)
+		return (-1);
+	while (*str++)
+		++len;
+	return (len);
+}
+
+/**
  * create_file - creates a file
  * @filename: A pointer to create
  * @text_content: A pointer to a string
  * Return: upon success, return 1, or -1
 */
-
-
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t b_written = 0;
 	int fd;
 
-	if (!filname)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
